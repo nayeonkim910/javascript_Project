a22f5095320bea972e5a4f85da01c585cff3ba1e
nayeonkim910 <yoni91059105@gmail.com>
2022-12-08 12:06:34 +0900

    ToDoList_Basic: delete
     delete기능 추가

--- Project_miniApp/App.js
+++ Project_miniApp/App.js
@@ -6,19 +6,30 @@ Btn.addEventListener('click',()=>{
     let IsValue=textBox.value===''? false : true;
     if(IsValue){
     const listItem = document.createElement('div');
+    const iconBox = document.createElement('div');
     const icon = document.createElement('div');
-    icon.innerHTML='<i class="fa-regular fa-xl fa-circle-check"></i>';
+    const iconTrash = document.createElement('div');
+    iconTrash.innerHTML='<i class="fa-solid fa-lg fa-trash"></i>';
+    icon.innerHTML='<i class="fa-regular fa-lg fa-circle-check"></i>';
     icon.setAttribute('class','iconDiv');
+    iconBox.setAttribute('class','iconBox')
+    iconTrash.setAttribute('class','iconDiv');
     listItem.setAttribute('class','listItem');
     listItem.textContent=`${textBox.value}`;
     textBox.value='';
     content.appendChild(listItem);
-    listItem.appendChild(icon);
+    listItem.appendChild(iconBox);
+    iconBox.appendChild(icon);
+    iconBox.appendChild(iconTrash);
+
         let IsLine;
          icon.addEventListener('click',()=>{
             listItem.style.textDecorationLine= IsLine===true?"none":"line-through";
             IsLine = IsLine === true? false : true;
             console.log(IsLine);
             });
+        iconTrash.addEventListener('click',()=>{
+            listItem.remove();
+        });
         }        
 })
