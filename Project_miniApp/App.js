34e3b6777b2f939ae13d73885751e7210bd204a3
nayeonkim910 <yoni91059105@gmail.com>
2022-12-10 23:47:54 +0900

    mini_app: style(purple_ver)

---
+++ Project_miniApp/App.js
@@ -0,0 +1,50 @@
+const content = document.querySelector(".content");
+const textBox = document.querySelector(".textBox");
+const Btn =document.querySelector(".Btn");
+
+
+Btn.addEventListener('click',()=>{
+    addListItem();
+})
+
+function addListItem(){
+    let IsValue=textBox.value===''? false : true;
+     if(IsValue){
+     const listItem = document.createElement('div');
+     const iconBox = document.createElement('div');
+     const icon = document.createElement('div');
+     const iconTrash = document.createElement('div');
+     iconTrash.innerHTML='<i class="fa-solid fa-lg fa-trash"></i>';
+     icon.innerHTML='<i class="fa-regular fa-lg fa-circle-check"></i>';
+     icon.setAttribute('class','iconDiv');
+     iconBox.setAttribute('class','iconBox')
+     iconTrash.setAttribute('class','iconDiv');
+     listItem.setAttribute('class','listItem');
+     listItem.textContent=`${textBox.value}`;
+     textBox.value='';
+     textBox.focus();
+     content.appendChild(listItem);
+     listItem.scrollIntoView({block: 'center'});
+
+     listItem.appendChild(iconBox);
+     iconBox.appendChild(icon);
+     iconBox.appendChild(iconTrash);
+
+            let IsLine;
+             icon.addEventListener('click',()=>{
+                listItem.style.textDecorationLine= IsLine===true?"none":"line-through";
+                IsLine = IsLine === true? false : true;
+                console.log(IsLine);
+                });
+            iconTrash.addEventListener('click',()=>{
+                listItem.remove();
+            });
+     }        
+} 
+
+textBox.addEventListener('keypress',(event)=>{
+    if(event.key==='Enter'){
+        addListItem();
+    }
+});
+
