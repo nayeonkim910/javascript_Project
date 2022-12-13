da202155dc711c52345414ebc81728431132ee95
nayeonkim910 <yoni91059105@gmail.com>
2022-12-14 02:28:53 +0900

    mini_app: JS modified

--- Project_miniApp/App.js
+++ Project_miniApp/App.js
@@ -17,24 +17,42 @@ function addItem(){
     textBox.focus();
   
 }
+let dataIdNum=0;
 function addItemBox(text){
     const listItem = document.createElement('div');
     listItem.setAttribute('class', 'listItem');
+    listItem.setAttribute('data-id', dataIdNum);
     listItem.innerHTML=`
     <div class="listItem__text">${text}</div>
     <div class="listItem__icon">
-        <div class="icon__check"><i class="fa-regular fa-lg fa-circle-check"></i></div>
-        <div class="icon__trash"><i class="fa-solid fa-lg fa-trash"></i></div>
+        <div class="icon__check"><i class="fa-regular fa-lg fa-circle-check CH" data-id=${dataIdNum}></i></div>
+        <div class="icon__trash"><i class="fa-solid fa-lg fa-trash TR" data-id=${dataIdNum}></i></div>
     </div>
     `;
+    dataIdNum++;
     return listItem;
 }
+
+content.addEventListener('click',(e)=>{
+    console.log(e);
+    const id = e.target.dataset.id; 
+    const item = document.querySelector(`.listItem[data-id="${id}"]`);
+    if(e.target.classList.contains('TR')){
+        item.remove();
+    }else if(e.target.classList.contains('CH')){
+        const line = item.style.textDecorationLine;
+        item.style.textDecorationLine = line ==='line-through'? 'none': 'line-through';
+    }
+});
 
 Btn.addEventListener('click',()=>{
     //value있는지 체크하고, 추가하도록
     addItem();
 });
 
+
+
+//enter에도 반응하도록
 textBox.addEventListener('keypress',(e)=>{
     if (e.key==='Enter') {
         addItem();
