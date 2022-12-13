98ee450ce95f71e179af4f4ec8b2f23351fdefec
nayeonkim910 <yoni91059105@gmail.com>
2022-12-14 01:15:08 +0900

    mini_app: JS (Editing)

--- Project_miniApp/App.js
+++ Project_miniApp/App.js
@@ -3,48 +3,41 @@ const textBox = document.querySelector(".textBox");
 const Btn =document.querySelector(".Btn");
 
 
-Btn.addEventListener('click',()=>{
-    addListItem();
-})
-
-function addListItem(){
-    let IsValue=textBox.value===''? false : true;
-     if(IsValue){
-     const listItem = document.createElement('div');
-     const iconBox = document.createElement('div');
-     const icon = document.createElement('div');
-     const iconTrash = document.createElement('div');
-     iconTrash.innerHTML='<i class="fa-solid fa-lg fa-trash"></i>';
-     icon.innerHTML='<i class="fa-regular fa-lg fa-circle-check"></i>';
-     icon.setAttribute('class','iconDiv');
-     iconBox.setAttribute('class','iconBox')
-     iconTrash.setAttribute('class','iconDiv');
-     listItem.setAttribute('class','listItem');
-     listItem.textContent=`${textBox.value}`;
-     textBox.value='';
-     textBox.focus();
-     content.appendChild(listItem);
-     listItem.scrollIntoView({block: 'center'});
-
-     listItem.appendChild(iconBox);
-     iconBox.appendChild(icon);
-     iconBox.appendChild(iconTrash);
-
-            let IsLine;
-             icon.addEventListener('click',()=>{
-                listItem.style.textDecorationLine= IsLine===true?"none":"line-through";
-                IsLine = IsLine === true? false : true;
-                console.log(IsLine);
-                });
-            iconTrash.addEventListener('click',()=>{
-                listItem.remove();
-            });
-     }        
-} 
-
-textBox.addEventListener('keypress',(event)=>{
-    if(event.key==='Enter'){
-        addListItem();
+function addItem(){
+    const text = textBox.value; //값있다면 넘겨주기
+    if(text==''){
+        textBox.focus();
+        return; //종료하기
     }
+    console.log(text);
+    const item = addItemBox(text); //return해주는거 넣기
+    content.appendChild(item);
+    item.scrollIntoView({block: 'center'});
+    textBox.value='';
+    textBox.focus();
+  
+}
+function addItemBox(text){
+    const listItem = document.createElement('div');
+    listItem.setAttribute('class', 'listItem');
+    listItem.innerHTML=`
+    <div class="listItem__text">${text}</div>
+    <div class="listItem__icon">
+        <div class="icon__check"><i class="fa-regular fa-lg fa-circle-check"></i></div>
+        <div class="icon__trash"><i class="fa-solid fa-lg fa-trash"></i></div>
+    </div>
+    `;
+    return listItem;
+}
+
+Btn.addEventListener('click',()=>{
+    //value있는지 체크하고, 추가하도록
+    addItem();
 });
 
+textBox.addEventListener('keypress',(e)=>{
+    if (e.key==='Enter') {
+        addItem();
+    }
+
+});
