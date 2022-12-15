22acf8865b17b43f0893de036731d9d25ebea6b4
nayeonkim910 <yoni91059105@gmail.com>
2022-12-15 21:38:16 +0900

    myProject: scrollIntoView

--- Project_game/src/main.js
+++ Project_game/src/main.js
@@ -1,7 +1,10 @@
 
 const body = document.querySelector('body');
-const coordi = document.querySelector('.coordi');
-// const home = document.querySelector('.home');
+const gameView = document.querySelector('.gameView');
+const secondBG = document.querySelector('.secondBG');
+const thirdBG = document.querySelector('.thirdBG');
+const other = document.querySelector('.otherBtn')
+const BG = new Audio("../bg.mp3");
 
 document.addEventListener('click',(e)=>{//위치꺼내고 
     const x=  e.clientX;
@@ -21,3 +24,20 @@ function makeStar(x, y){
     console.log('위치넣어짐');
     return star;
 }
+
+gameView.addEventListener('click',()=>{
+    BG.play();
+    seamless.scrollIntoView(secondBG,{behavior: "smooth", block: "center"});
+    //스크롤내려서 두번째 화면 보이게 만들기
+});
+other.addEventListener('click',()=>{
+    seamless.scrollIntoView(thirdBG,{behavior: "smooth", block: "center"});
+})
+
+//게임화면 나오기
+//게임 시작
+//타이머 만들기 
+
+// secondBG.scrollIntoView();
+
+// 게임시작하는 함수 만들기 gameStart();
