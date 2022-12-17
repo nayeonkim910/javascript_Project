65e21c0a5cdf81b0c9ee68fadfec6f040dd34c62
nayeonkim910 <yoni91059105@gmail.com>
2022-12-17 19:39:23 +0900

    CSS:cursor

--- Project_game/src/main.js
+++ Project_game/src/main.js
@@ -6,19 +6,11 @@ const body = document.querySelector('body');
     const gameC = document.querySelector(".gameImgC");
     
 
-
-    // -----------------second
+//-----------------second-----------------
 const secondBG = document.querySelector('.secondBG');
     const checkBox = document.querySelectorAll(".checkBox");
 
-
-    // -----------------third
-const thirdBG = document.querySelector('.thirdBG');
-// const other = document.querySelector('.otherBtn')
-
-
-
-// -----------------fourth
+//----------------fourthd-------------------
 const fourthBG = document.querySelector(".fourthBG");
 // const fourthRect = fourthBG.getBoundingClientRect();
 
@@ -28,18 +20,15 @@ const f_BtnStart = document.querySelector(".f_BtnStart");
 
     const f_gameView = document.querySelector(".f_gameView");    
     const viewRect = f_gameView.getBoundingClientRect();
-    
+    const lifeImg = document.querySelectorAll(".lifeSet__item");
 
-//---------------sound
+//---------------sound-----------------
 const BG = new Audio("../sound/bg.mp3");
 const SoundWin = new Audio("../sound/game_win.mp3");
 const SoundCheck = new Audio("../sound/carrot_pull.mp3");
 const SoundAlert = new Audio("../sound/alert.wav");
 const homeBG = new Audio("../sound/theduel.mp3");
-const BtnStopBG = document.querySelector(".BtnStopBG");
-   
-
- 
+const BtnStopBG = document.querySelector(".BtnStopBG");    
 
 BtnStopBG.addEventListener('click',()=>{
     BG.pause();
@@ -51,49 +40,34 @@ gameA.addEventListener('click',()=>{
     //스크롤내려서 두번째 화면 보이게 만들기
 });
 
-
-
 gameC.addEventListener('click',()=>{
     homeBG.play();
     seamless.scrollIntoView(fourthBG,{behavior: "smooth", block: "center"});
 });
 
 //게임화면 나오기
-//게임 시작
-//타이머 만들기 
-
-// secondBG.scrollIntoView();
-
 // 게임시작하는 함수 만들기 gameStart();
-
- 
 let ch=0;
     checkBox.forEach((item)=>{
-             
              item.addEventListener('click',()=>{
                      item.style.opacity=1;
                      ch ++;
                      SoundCheck.play();
                      if(ch==8){
                         SoundWin.play();
-                       
                     }
                  });
-           
         });
+
 function randomNum(min, max){
     return Math.random()*(max-min) +min;
 }
-
-
 //----------------------------------------------4번 화면 당근찾기 ----------
- 
-// 타이머 몇초 할지 설정하고, 첫화면에 띄우기 
-//타이머 동작하기 ----------------------------------------
+let userLife = 3;
 function showTimer(second){
     let min =Math.floor(second/60);
     timer.innerHTML=`${min<10?0:''}${min}:${second<10?0:''}${second}`;
-    MakeIntervalItems(second);
+    MakeIntervalItems(second,userLife);
 }
 
 //타이머 -1씩 인터벌 동작
@@ -102,22 +76,14 @@ function StartCountdown(second){
          CountdownId =setInterval(()=>{  
             second--;  
             showTimer(second);
-            if(second<=0){
-                clearInterval(CountdownId);        
+            if(second<=0||userLife==0){
+                clearInterval(CountdownId);
+                const done = document.querySelector(".f_gameView__done");
+                done.style.display='inline';       
           }
         },1000);
           
 }
-
-
-
-
-
-
-
-
-
-//-------------------
 function makeItem(className,count,ImgSrc){
        for(i=0; i<count; i++){
             const MIN_X=0;
@@ -134,63 +100,52 @@ function makeItem(className,count,ImgSrc){
             f_gameView.appendChild(item);
     }
 };
-
 function MakeItems(){
     makeItem('carrot',1,'img/carrot.png');
     makeItem('bug',1,'img/bug.png'); 
 }
-//1초마다 makeItem틀에 /설정 넣어서 당근 생성하기 함수
-
+function lifeDone(){
+    userLife--; //2,1,0
+    lifeImg[userLife].style.display='none';
+}
 function MakeIntervalItems(sec){
-        console.log(sec);
-        let MakeItemsId;
+    let MakeItemsId;
         MakeItemsId=setInterval(MakeItems,1000); //point 생성중
-        setTimeout(()=>{clearInterval(MakeItemsId); getAllImgRemove();  },sec*1000);
-        
-} 
-
+    setTimeout(()=>{clearInterval(MakeItemsId); getAllImgRemove(); },sec*1000);
+    } 
 function timerDone(){
     timer.style.display ='none';
 }
-
 let point=0;
 function showPoint(){
     score.style.display = 'inline'
     score.innerHTML = `Your Score: ${point}`;
     point++;
 }
-
-let userLife = 3;
-
-function lifeDone(){
-    userLife--; //2,1,0
-    const lifeImg = document.querySelectorAll(".lifeSet__item");
-    lifeImg[userLife].style.display='none';
-    
-    
+function getAllImgRemove(){
+    const arrC = document.querySelectorAll(".carrot");
+    const arrB = document.querySelectorAll(".bug");
+    arrC.forEach(carrot=>carrot.style.display='none');
+    arrB.forEach(bug=>bug.style.display='none');
 }
 //네번째 페이지 Click 이벤트 위임하기 
 fourthBG.addEventListener('click',(event)=>{
     let second=10;
     let item = event.target;
     let className =event.target.className; 
-    //item고르면 삭제하고 
-    
     if(item.nodeName=='IMG'){
-
                       if(className==='carrot'){
                           SoundCheck.play();
                            showPoint();
                            event.target.remove();               
                       }
-                  
                        else if(className=='bug'){
                            SoundAlert.play();
                            lifeDone();
-                           console.log(userLife);
+                           console.log('유저라잎',userLife);
+                           event.target.remove();
                            }
-                       }
-         //시작버튼 누르면 시작표시 사라지고, 게임 시작
+        }
          if(className=='f_BtnStart'){
             showPoint();
             MakeItems();
@@ -198,9 +153,9 @@ fourthBG.addEventListener('click',(event)=>{
             showTimer(second);//=> MakeIntervalItems(second)
             StartCountdown(second);
         }
-        if(className=='BtnStopBG'){
-            homeBG.pause();
-        }
+         if(className=='BtnStopBG'){
+             homeBG.pause();
+         }
 });                        
 //--------------------------------------
 
@@ -208,9 +163,3 @@ fourthBG.addEventListener('click',(event)=>{
 
 
 
-function getAllImgRemove(){
-    const arrC = document.querySelectorAll(".carrot");
-    const arrB = document.querySelectorAll(".bug");
-    arrC.forEach(carrot=>carrot.style.display='none');
-    arrB.forEach(bug=>bug.style.display='none');
-}
